4b1a83ee7be958d9a7c2d1349cbb678b1ddf3e32
mehdi shirazi <msh199271@gmail.com>
2020-08-27 14:28:59 +0430

    Fixing bug in order to show items scycronizly

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,16 +2,29 @@ import React from 'react'
 import "./Header.css"
 import Postman from "../Img/Postman.png"
 
-const Header = () => {
-    return (
-        <div className="flex-container">
-            <img src={Postman} className="flex-item-one"></img>
-            <nav className="flex-item-two">
-                <nav className="api">APIs</nav>
-                <nav className="curl">curl</nav>
-            </nav>
-        </div>
-    )
+class Header extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoadingImg: false
+        }
+    }
+
+componentDidMount() {
+    this.setState({isLoadingImg: true})
+}
+
+    render() {
+        return (
+            <div className="flex-container">
+                {this.state.isLoadingImg ? <img src={Postman} className="flex-item-one"></img> : null}
+                <nav className="flex-item-two">
+                    <nav className="api">APIs</nav>
+                    <nav className="curl">curl</nav>
+                </nav>
+            </div>
+        )
+    }
 }
 
 export default Header
